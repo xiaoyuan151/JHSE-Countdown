e3e0c06c69faf84808c27ae95603584495d4d792
xiaoyuan151 <xiaoyuan2233666@163.com>
2023-07-29 12:20:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+<div align="center">
+  <h1>中考倒计时</h1>
+</div>
+
+中考倒计时，支持毫秒级倒计时（~~增加压迫感~~），可配合Wallpaper Engine等软件使用\
+示例网站：<https://jhsec.xiaoyuan151.xyz> \
+参数示例网站（不显示毫秒）：<https://jhsec.xiaoyuan151.xyz/index.html?no_ms=t>
+
+```
+支持传入的参数：
+  参数 ｜ 支持的数据 ｜ 参数解释
+  page_title ｜ str ｜ 设置网页标题
+  no_bg ｜ t/f ｜ 不显示网页背景
+  pic_bg ｜ str ｜ 设置网页背景图片（所有图片必须在文件夹bgs下）
+  vid_bg ｜ str ｜ 设置网页背景视频（所有视频必须在文件夹bgs下）
+  aid_bg ｜ str ｜ 设置网页背景音乐（所有音乐必须在文件夹bgs下）
+  title ｜ str ｜ 设置标题文本（如“距中考还有：”）
+  title ｜ str ｜ 设置文本（如“中考加油!”）
+  no_ms ｜ t/f ｜ 不显示毫秒
+  no_s ｜ t/f ｜ 不显示秒
+  no_m ｜ t/f ｜ 不显示分
+  no_h ｜ t/f ｜ 不显示时
+  no_d ｜ t/f ｜ 不显示天
+```
---
+++ javascript.js
@@ -0,0 +1,85 @@
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [undefined, ""])[1].replace(/\+/g, '%20')) || null;
+}
+
+if (getURLParameter("page_title") != undefined) {
+    document.title = getURLParameter("page_title")
+} else {
+    document.title = "中考倒计时"
+}
+if (getURLParameter("vid_bg") != undefined) {
+    document.getElementById("v1").src = "bgs/" + getURLParameter("vid_bg");
+    document.getElementById("v1").play()
+} else {
+    document.getElementById("v1").src = "bgs/bg.mp4";
+    document.getElementById("v1").play()
+}
+if (getURLParameter("aid_bg") != undefined) {
+    document.getElementById("a1").src = "bgs/" + getURLParameter("aid_bg");
+    document.getElementById("a1").play()
+} else {
+    document.getElementById("a1").src = "bgs/bg.mp3";
+    document.getElementById("a1").play()
+}
+if (getURLParameter("pic_bg") != undefined) {
+    document.body.style.background = "url(bgs/" + getURLParameter("pic_bg") + ") no-repeat"
+    document.body.style.backgroundSize = "100% 100%"
+    document.body.style.backgroundAttachment = "fixed"
+} else {
+    document.body.style.background = "url(bgs/bg.jpg) no-repeat"
+    document.body.style.backgroundSize = "100% 100%"
+    document.body.style.backgroundAttachment = "fixed"
+}
+if (getURLParameter("no_ms") == "t") {
+    document.getElementById("v1").src = "";
+    document.getElementById("a1").src = "";
+    document.body.style.background = ""
+}
+window.onload = function starttime() {
+    let h1 = document.getElementById('h1')
+    time(h1, '2024/6/25');
+    setTimeout(starttime, 1);
+}
+
+function time(obj, futimg) {
+    let html;
+    const nowtime = new Date().getTime();
+    const futruetime = new Date(futimg).getTime();
+    const msec = futruetime - nowtime;
+    const millisecond = parseInt(msec % 1000);
+    const time = (msec / 1000);
+    const day = parseInt(time / 86400);
+    const hour = parseInt(time / 3600) - 24 * day;
+    const minute = parseInt(time % 3600 / 60);
+    const second = parseInt(time % 60);
+    if (getURLParameter("title") != undefined) {
+        html = "<br><div>" + getURLParameter("title") + "</div><br>";
+    } else {
+        html = "<br><div>距2024年中考还有：</div><br>";
+    }
+    if (getURLParameter("no_d") != "t") {
+        html = html + day + "天"
+    }
+    if (getURLParameter("no_h") != "t") {
+        html = html + hour + "小时"
+    }
+    if (getURLParameter("no_m") != "t") {
+        html = html + minute + "分"
+    }
+    if (getURLParameter("no_s") != "t") {
+        html = html + second + "秒"
+    }
+    if (getURLParameter("no_ms") != "t") {
+        html = html + millisecond + "毫秒"
+    }
+    if (getURLParameter("text") != undefined) {
+        html = html + "<br><span>" + getURLParameter("text") + "</span></br>";
+    } else {
+        html = html + "<br><span>中考加油! </span></br>"
+    }
+    if (futruetime <= nowtime) {
+        obj.innerHTML = "<div class='blink'>倒计时结束！</div>"
+    } else {
+        obj.innerHTML = html
+    }
+}
